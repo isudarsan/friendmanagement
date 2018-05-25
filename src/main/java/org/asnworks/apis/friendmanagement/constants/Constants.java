040e291ab297da61bb962da347113af28794aef6
Sudarshan <sudarsan.ambatipudi@capgemini.com>
2018-05-25 16:49:21 +0530

    Added Constants

---
+++ src/main/java/org/asnworks/apis/friendmanagement/constants/Constants.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.asnworks.apis.friendmanagement.constants;
+
+/**
+ * @author sudambat
+ */
+public class Constants {
+
+    public static final String JPA_REPO_PACKAGE = "org.asnworks.apis.friendmanagement.repo";
+
+    public static final String ENTITY_SCAN_PACKAGE = "org.asnworks.apis.friendmanagement.domain";
+
+    public static final String SERVICE_COMPONENT_SCAN_PACKAGE = "org.asnworks.apis.friendmanagement.service";
+
+    public static final String EMAIL_REGEX_MATCHER = "([a-z0-9_.-]+)@([a-z0-9_.-]+[a-z])";
+
+    public static final String UNKNOWN_ERROR_CODE = "5000";
+
+    public static final String UNKNOWN_ERROR_MESSAGE = "Unknown Error";
+}
